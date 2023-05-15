00f63aa1be552f09eb5e7b2738d05085ef803c1e
Dujd6 <454587515@qq.com>
2023-05-15 11:20:25 +0800

    test Git

---
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.gittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.e("aaaa", "test");
+    }
+}
+
+
